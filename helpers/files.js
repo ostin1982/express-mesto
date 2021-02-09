d2ab792ff296740d1a14e8f252018d847c98f1b3
Denis Sokolov <ostin1982@yandex.ru>
2021-02-09 16:26:07 +0300

    Двеннадцатый спринт. Первый коммит

--- helpers/files.js
+++ helpers/files.js
@@ -4,7 +4,7 @@ const fsPromises = require('fs').promises;
 const getDataFromFile = (pathToFile) => {
   return fsPromises.readFile(pathToFile, {encoding: 'utf-8'})
   .then(data => JSON.parse(data))
-  .catch(error => console.error(error))
+  .catch(error => console.log(error))
 };
 
 
